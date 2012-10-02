101c045710024fd9af617bbbc1d4cef8b33678f1
Eric Clemmons <eric@smarterspam.com>
2012-10-01 22:51:34 -0500

    Refactored Generator to not write invalid paths

--- src/Skeleton/Generator/Generator.php
+++ src/Skeleton/Generator/Generator.php
@@ -12,6 +12,8 @@ class Generator
 
     private $destination;
 
+    private $expression ='/{{{?([\w.]+)}}}?/';
+
     public function __construct(Skeleton $skeleton, $source, $destination)
     {
         $this->skeleton     = $skeleton;
@@ -37,43 +39,71 @@ class Generator
                 return false;
             }
 
-            $template = str_replace(array($this->source.'/', '.mustache'), null, $path);
-            $contents = file_get_contents($path);
+            $contents = $this->getParsedTemplate($file);
 
-            if ($file->getExtension() === 'mustache') {
-                $contents = $this->parse($contents);
+            try {
+                $target     = $this->getParsedTemplatePath($file);
+                $parentDir  = dirname($target);
+
+                if (!is_dir($parentDir) && !mkdir($parentDir, 0775, true)) {
+                    throw new \Exception('Unable to create directory '.$parentDir);
+                }
+
+                if (!file_put_contents($target, $contents)) {
+                    throw new \Exception('Unable to write to '.$target);
+                }
+
+                $generated[] = $target;
+            } catch (\InvalidArgumentException $e) {
+                // Path is invalid, likely because a config value isn't set
             }
-
-            $target = $this->parse(sprintf('%s/%s', $this->destination, $template));
-
-            if (is_dir($target)) {
-                continue;
-            }
-
-            $parentDir = dirname($target);
-
-            if (!is_dir($parentDir) && !mkdir($parentDir, 0775, true)) {
-                throw new \Exception('Unable to create directory '.$parentDir);
-            }
-
-            if (!file_put_contents($target, $contents)) {
-                throw new \Exception('Unable to write to '.$target);
-            }
-
-            $generated[] = $target;
         }
 
         return $generated;
     }
 
-    public function parse($template)
+    public function getParsedTemplate(\SplFileInfo $file)
     {
-        $skeleton = $this->skeleton;
+        $contents = file_get_contents($file->getRealPath());
 
-        $parsed = preg_replace_callback('/{{{?([\w.]+)}}}?/', function($matches) use ($skeleton) {
-            return $skeleton->get($matches[1]);
-        }, $template);
+        if ($file->getExtension() === 'mustache') {
+            $skeleton = $this->skeleton;
 
-        return $parsed;
+            $contents = preg_replace_callback($this->expression, function($matches) use ($skeleton) {
+                return $skeleton->get($matches[1]);
+            }, $contents);
+        }
+
+        return $contents;
+    }
+
+    public function getParsedTemplatePath(\SplFileInfo $file)
+    {
+        $path = $file->getRealPath();
+
+        // Remove skeleton path
+        $path = str_replace($this->source.'/', null, $path);
+
+        // Remove mustache extension
+        $path = str_replace('.mustache', null, $path);
+
+        // Prepend destination directory
+        $path = sprintf('%s/%s', $this->destination, $path);
+
+        if (preg_match($this->expression, $path)) {
+            $skeleton = $this->skeleton;
+
+            $path = preg_replace_callback($this->expression, function($matches) use ($skeleton) {
+                $value = $skeleton->get($matches[1]);
+
+                if (null === $value) {
+                    throw new \InvalidArgumentException('No value exists for '.$matches[0]);
+                }
+
+                return $value;
+            }, $path);
+        }
+
+        return $path;
     }
 }
